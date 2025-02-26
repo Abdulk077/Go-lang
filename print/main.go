b246384fe7aff6124e0bd9e20b55379541522135
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-02-27 00:28:42 +0530

    Learning About the println and printf in go

---
+++ print/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	age :=20
+	name := "Abdul"
+	heights := 5.8
+	// println first print the every conetnt shifted the curser to new line
+	fmt.Println("Name: ", name, "Heights: ", heights, "Age: ", age)
+	// Printf print the every content in the same line
+	fmt.Printf("age is %d",age)
+
+}
