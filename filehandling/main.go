a3cc122e609474ba225647d6294970f22c791fc9
Abdulk077 <Khanabdulwadud3@gmail.com>
2025-03-02 14:09:41 +0530

    Buffer in go Read And Creating A file in Go-Lang

---
+++ filehandling/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// creating a file using os
+	//file, err := os.Create("examples.txt")
+	/*if err != nil {
+		fmt.Println("Error While Creating File: ", err)
+		return
+	}
+	// closing a file at the end of program
+	defer file.Close()
+	content := "Hello World By Abdul Wadud Khan"
+	// Writing to a file and showing te no of byte
+	byte, errors := io.WriteString(file,content)
+	if errors != nil{
+		fmt.Println("error", errors)
+		return
+	}
+	*/
+	// opening a file
+	file, err := os.Open("examples.txt")
+	if err != nil {
+		fmt.Println("Error While Creating File: ", err)
+		return
+	}
+	defer file.Close()
+	// to read data we need to use buffer
+	buffer := make([]byte,1024)
+	for {
+		n, err := file.Read(buffer)
+		// entering the  end of file
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error While Reading File: ",err)
+			return
+		}
+		fmt.Println(string(buffer[:n]))
+
+	}
+	
+	fmt.Println("Successfully Read the file")
+	//fmt.Println(byte)
+
+}
